a298f0be5e7d4b4c70468dacf893a6c66207c3f7
Angore-78 <allan.angore@gmail.com>
2025-08-15 14:50:37 +0300

    correcting alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store CREATE TABLE Books(PRIMARY KEY (book_id),title VARCHAR(130),FOREIGN KEY(author_id),price, DATE(publication_date))
-CREATE TABLE Authers(PRIMARY KEY(author_id),auther_name VARCHAR(215)) CREATE TABLE customers(PRIMARY KEY (customer_id),
+CREATE TABLE Authors(PRIMARY KEY(author_id),auther_name VARCHAR(215)) CREATE TABLE customers(PRIMARY KEY (customer_id),
 customer_name VARCHAR(215),email VARCHAR(215),
 TEXT(adress)) CREATE TABLE Orders(PRIMARY KEY(order_id),FOREIGN KEY(customer_id),DATE(order_date)) CREATE TABLE Order_Details(
     PRIMARY KEY(orderdetailid),FOREIGN KEY(order_id),FOREIGN KEY(book_id),quantity
