515f12a461aaab063422a507b0eeca31809e2938
Angore-78 <allan.angore@gmail.com>
2025-08-16 17:19:56 +0300

    task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 USE alx_book_store
-SELECT * FROM Customer
-INSERT(`customer_id`,`customer_name` = `Blessing Malik`,`email` = `bmalik@sandtech.com`,
+SELECT * FROM customer
+INSERT INTO  customer(`customer_id`,`customer_name` = `Blessing Malik`,`email` = `bmalik@sandtech.com`,
 `address` = `124 Happiness  Ave.`)
 VALUES(`2`,`Blessing Malik`,`bmalik@sandtech.com`,`124 Happiness  Ave.`)
 VALUES(`3`,`Obed Ehoneah`,`eobed@sandtech.com`,`125 Happiness  Ave.`)
