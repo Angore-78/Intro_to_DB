41740d70e3d95b080b2fa8c30410a76c31a47489
Angore-78 <allan.angore@gmail.com>
2025-08-16 16:25:46 +0300

    correction to MySqlServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,11 +1,11 @@
 import mysql.connector 
+configure = {'host':'localhost','user':'Allan.A','password':'B4#120!9ed','database':'alx_book_store'}
+
 try:
-    mydb = mysql.connector.connect (
-        host = 'localhost',
-        user = 'Allan.A',
-        password = 'B4#120!9ed',
-        database = 'alx_book_store'
-        )
+    mydb =mysql.connector.connect(**configure)
+    cursor = mydb.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    
     print("Database 'alx_book_store' created successfully!")
 except mysql.connector.Error as e :
     print(f'Encountered Error:{e}')
