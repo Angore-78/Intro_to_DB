2c5a1642e33130c731bfcfc303aefd10541e03cc
Angore-78 <allan.angore@gmail.com>
2025-08-16 17:38:27 +0300

    correction to alx_book_store.sql and task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,5 +1,4 @@
 USE alx_book_store CREATE TABLE Books(PRIMARY KEY (book_id),title VARCHAR(130),FOREIGN KEY(author_id),price, DATE(publication_date))
 CREATE TABLE Authors(PRIMARY KEY(author_id ),author_name VARCHAR(215)) CREATE TABLE Customers(PRIMARY KEY (customer_id ),
-customer_name VARCHAR(215),email VARCHAR(215),address TEXT) CREATE TABLE Orders(PRIMARY KEY(order_id INT),FOREIGN KEY(customer_id INT) REFERENCES Customers(Customer_id),DATE(order_date)) CREATE TABLE Order_Details(
-    PRIMARY KEY(orderdetailid),FOREIGN KEY(order_id),FOREIGN KEY(book_id),quantity
-)
+customer_name VARCHAR(215),email VARCHAR(215),address TEXT) CREATE TABLE Orders(order_id INT, customer_id INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)) CREATE TABLE Order_Details(book_id INT, quantity DOUBLE, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
