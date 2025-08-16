88c2c7dfe097655b696067686d79b030fb335b28
Angore-78 <allan.angore@gmail.com>
2025-08-16 17:18:33 +0300

    task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,7 @@
+USE alx_book_store
+SELECT * FROM Customer
+INSERT(`customer_id`,`customer_name` = `Blessing Malik`,`email` = `bmalik@sandtech.com`,
+`address` = `124 Happiness  Ave.`)
+VALUES(`2`,`Blessing Malik`,`bmalik@sandtech.com`,`124 Happiness  Ave.`)
+VALUES(`3`,`Obed Ehoneah`,`eobed@sandtech.com`,`125 Happiness  Ave.`)
+VALUES(`4`,`Nehemial Kamolu`,`nkamolu@sandtech.com`,`126 Happiness  Ave.`)
